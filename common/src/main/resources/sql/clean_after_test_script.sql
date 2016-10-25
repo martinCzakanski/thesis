f5f5ce051ed775b8d4d266a8834113db4f5bd0f6
MACZAKAN <martin.czakanski@gmail.com>
2016-10-25 18:05:40 +0200

    thesis - DB scrips

---
+++ common/src/main/resources/sql/clean_after_test_script.sql
@@ -0,0 +1,8 @@
+delete from users where user_id > 1000
+DBCC CHECKIDENT ('[users]', RESEED, 1000)
+
+delete from session where session_id > 1000
+DBCC CHECKIDENT ('[session]', RESEED, 1000)
+
+delete from message where 1 =1
+DBCC CHECKIDENT ('[message]', RESEED, 0)
